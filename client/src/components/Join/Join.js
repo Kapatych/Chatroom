ed2e677428048480014cda05aa3020c65c335293
Kapatych <onetruestory@ya.ru>
2020-04-01 20:47:44 +0300

    Deleted unnecessary files, create Join and Chat component, add node-sass, tuned server side - add join and sendMessage io event, add users helping functions

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import './Join.scss'
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div className='join'>
+      <div className='join__container'>
+        <h1 className='heading'>Join</h1>
+        <input value={name}
+               className='join__input'
+               type='text'
+               placeholder='Name'
+               onChange={event => setName(event.target.value)}/>
+        <input value={room}
+               className='join__input'
+               type='text'
+               placeholder='Room'
+               onChange={event => setRoom(event.target.value)}/>
+        <Link to={`/chat?name=${name}&room=${room}`}
+              onClick={event => (!name || !room) ? event.preventDefault() : null}>
+          <button className='button join__button' type='submit'>Sign In</button>
+        </Link>
+      </div>
+    </div>
+  )
+};
+
+export default Join;
---
+++ server/index.js
@@ -0,0 +1,42 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+io.on('connection', (socket) => {
+    socket.on('join', ({name, room}, callback) => {
+        const {error, user} = addUser({id: socket.id, name, room});
+
+        if (error) return callback(error);
+
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}!`});
+
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined!`});
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User had left');
+    })
+});
+
+
+app.use(router);
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
